f70c1cdbdfade56fe7540a31fa17d8debf27a350
Nickske666 <nick.van.den.broeck@hotmail.com>
2017-04-10 09:26:35 -0400

    Some minor changes in genvalidity-hspec-hashable

--- genvalidity-hspec-hashable/test/Test/Validity/HashableSpec.hs
+++ genvalidity-hspec-hashable/test/Test/Validity/HashableSpec.hs
@@ -3,7 +3,7 @@
 
 -- | Standard 'Spec's for 'Hashable' instances.
 --
--- You will need @Hashable@ to use these.
+-- You will need @TypeApplications@ to use these.
 module Test.Validity.HashableSpec where
 
 import Test.Hspec
@@ -19,50 +19,50 @@ spec = do
     hashableSpecOnValid @Double
     hashableSpec @Int
     hashableSpecOnArbitrary @Int
-    hashableSpec @HashTrue
+    hashableSpec @HashableValid
     failsBecause ("Two equal elements aren't hashed to the same value!") $
-        hashableSpec @HashFalse
+        hashableSpec @HashableInvalid
 
-newtype HashTrue = HashTrue Int
+newtype HashableValid = HashableValid Int
     deriving (Show, Generic)
 
-hT :: Int -- Number used in the definition of HashTrue
+hT :: Int -- Number used in the definition of HashableValid
 hT = 7
 
-instance Eq HashTrue where
-    (==) (HashTrue x) (HashTrue y) = (x `mod` hT) == (y `mod` hT)
+instance Eq HashableValid where
+    (==) (HashableValid x) (HashableValid y) = (x `mod` hT) == (y `mod` hT)
 
-instance Hashable HashTrue where
-    hashWithSalt n (HashTrue a) = (int ^ expo) `mod` hT
+instance Hashable HashableValid where
+    hashWithSalt n (HashableValid a) = (int ^ expo) `mod` hT
                                where int = 1 + (a `mod` hT)
                                      expo = 1 + (n `mod` hT)
 
-instance Validity HashTrue
+instance Validity HashableValid
 
-instance GenValid HashTrue
+instance GenValid HashableValid
 
-instance GenUnchecked HashTrue
+instance GenUnchecked HashableValid
 
-newtype HashFalse = HashFalse Int
+newtype HashableInvalid = HashableInvalid Int
     deriving (Show, Generic)
 
-hF :: Int -- Numbers used in the definition of HashFalse
+hF :: Int -- Numbers used in the definition of HashableInvalid
 hF = 8
 hM :: Int
 hM = 3
 
-instance Eq HashFalse where
-    (==) (HashFalse x) (HashFalse y) = (x `mod` hF) == (y `mod` hF)
+instance Eq HashableInvalid where
+    (==) (HashableInvalid x) (HashableInvalid y) = (x `mod` hF) == (y `mod` hF)
 
-instance Hashable HashFalse where
-    hashWithSalt n (HashFalse a) = (int ^ expo) `mod` hM
+instance Hashable HashableInvalid where
+    hashWithSalt n (HashableInvalid a) = (int ^ expo) `mod` hM
                                 where int = 1 + (a `mod` hM)
                                       expo = 1 + (n `mod` hM)
 
-instance Validity HashFalse
+instance Validity HashableInvalid
 
-instance GenValid HashFalse
+instance GenValid HashableInvalid
 
-instance GenUnchecked HashFalse
+instance GenUnchecked HashableInvalid
 
 
