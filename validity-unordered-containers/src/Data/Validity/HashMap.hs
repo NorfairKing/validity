f0c27c81f9b72e609f5675cd7f385732ef47ae64
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-07-29 08:56:47 +0200

    First draft of validity-unordered-containers

---
+++ validity-unordered-containers/src/Data/Validity/HashMap.hs
@@ -0,0 +1,16 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Data.Validity.HashMap where
+
+import Data.Validity
+
+import Data.Hashable (Hashable)
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HM
+
+-- | A 'HashMap' of things is valid if all the keys and values are valid.
+--
+-- The 'unordered-containers' package does not export any more functionality
+-- concerning a 'HashMap', so no more accurate validity instance can be made.
+instance (Hashable k, Validity k, Validity v) => Validity (HashMap k v) where
+    isValid m = all isValid (HM.toList m)
---
+++ validity-unordered-containers/src/Data/Validity/HashSet.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Data.Validity.HashSet where
+
+import Data.Validity
+import Data.Validity.HashMap ()
+
+import Data.HashSet (HashSet)
+import qualified Data.HashSet as HS
+import Data.Hashable (Hashable)
+
+-- | A 'HashSet' of things is valid if all the elements are valid and
+-- the underlying 'HashMap' is valid.
+instance (Hashable v, Validity v) => Validity (HashSet v) where
+    isValid hs = isValid $ HS.toMap hs
