452557750cb404a4f2217629f73d15d89e0e8822
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2018-06-20 10:57:26 +0200

    another attempt

--- genvalidity-containers/test/Test/Validity/Containers/MapSpec.hs
+++ genvalidity-containers/test/Test/Validity/Containers/MapSpec.hs
@@ -5,11 +5,20 @@ module Test.Validity.Containers.MapSpec where
 
 import Test.Hspec
 
-import Data.GenValidity.Map ()
+import Data.GenValidity.Map
 import Data.Map (Map)
 import Test.Validity.GenValidity
+
 
 spec :: Spec
 spec = do
     genValidSpec @(Map Int Double)
     genValiditySpec @(Map Double Double)
+#if MIN_VERSION_containers(0,5,9)
+    describe "genStructurallyInvalidSet" $
+        it "produces invalid sets" $
+        genGeneratesInvalid
+            (genStructurallyInvalidMap @Double @Double)
+            (const [])
+#endif
+
--- genvalidity-containers/test/Test/Validity/Containers/SetSpec.hs
+++ genvalidity-containers/test/Test/Validity/Containers/SetSpec.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Test.Validity.Containers.SetSpec where
 
 import Test.Hspec
 
-import Data.GenValidity.Set ()
+import Data.GenValidity.Set
 import Data.Set (Set)
 import Test.Validity.GenValidity
 
@@ -12,3 +13,10 @@ spec :: Spec
 spec = do
     genValidSpec @(Set Int)
     genValiditySpec @(Set Double)
+#if MIN_VERSION_containers(0,5,9)
+    describe "genStructurallyInvalidSet" $
+        it "produces invalid sets" $
+        genGeneratesInvalid
+            (genStructurallyInvalidSet @Double)
+            (const [])
+#endif
