82019db903f98bd49d5db2fedff1ab7546bc074f
Tom Sydney Kerckhove <syd@cs-syd.eu>
2018-07-26 18:27:29 +0200

    Started with standard specs for lenses

--- genvalidity-hspec-optics/test/Test/Validity/OpticsSpec.hs
+++ genvalidity-hspec-optics/test/Test/Validity/OpticsSpec.hs
@@ -9,5 +9,20 @@ import Test.Hspec
 import Test.Validity
 import Test.Validity.Optics
 
+import Lens.Micro
+
 spec :: Spec
-spec = pure ()
+spec = do
+    lensSpecOnValid
+        ((_2) :: Lens (Double, Double) (Double, Double) Double Double)
+    lensSpec ((_2) :: Lens (Int, Int) (Int, Int) Int Int)
+    lensSpecOnArbitrary
+        ((_2) :: Lens (Double, Double) (Double, Double) Double Double)
+    lensSpecOnGen
+        ((_2) :: Lens (Double, Double) (Double, Double) Double Double)
+        (abs <$> genValid)
+        "positive valid doubles"
+        (filter (0.0 >=) . shrinkValid)
+        ((,) <$> (negate . abs <$> genValid) <*> (negate . abs <$> genValid))
+        "tuples of negative valid doubles"
+        (const [])
