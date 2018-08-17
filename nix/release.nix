03eb4454b85c45ffdcd60bc1afded5f501a6a0cd
Tom Sydney Kerckhove <syd@cs-syd.eu>
2018-08-18 01:26:39 +0200

    no need for the complicated workaround

--- nix/release.nix
+++ nix/release.nix
@@ -1,12 +1,2 @@
 let pkgs = import (../default.nix);
-in {
-  release = 
-    with pkgs.haskellPackages;
-    pkgs.stdenv.mkDerivation {
-      name = "validity-release";
-      buildInputs = pkgs.lib.attrsets.attrValues pkgs.validityPackages;
-      buildCommand= ''
-        cp -r ${genvalidity-hspec} $out
-      '';
-    };
-}
+in pkgs.validityPackages
