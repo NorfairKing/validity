400fae2192eedb46782217283478dd44b051da61
Tom Sydney Kerckhove <syd@fpcomplete.com>
2019-05-08 12:52:37 +0200

    new versions on travis

--- nix/nixpkgs-version.nix
+++ nix/nixpkgs-version.nix
@@ -2,6 +2,6 @@
 # nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/07723f14bb132f6d65c74b953bef39cd80457273.tar.gz
 
 {
-  rev    = "44ae35170f72c839fa38cee130ede8ed5c245a12";
-  sha256 = "0vd8v4mqz3ccaqjn568h8pa2gm69ii3r3n5hqmd6d89x30a3jffb";
+  rev    = "82d2c663b4dffbd635ed694bcc301284987b8097";
+  sha256 = "1yqa0binyj8c5sighi7is9kv7j1890sssbjqxslfq7jc02xxmglh";
 }
