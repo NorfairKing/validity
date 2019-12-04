d00dca84e98be3c5ed8599da8aca9419f3c9f03f
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-12-04 19:59:13 +0100

    polishing

--- genvalidity-criterion/src/Data/GenValidity/Criterion.hs
+++ genvalidity-criterion/src/Data/GenValidity/Criterion.hs
@@ -2,7 +2,13 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
-module Data.GenValidity.Criterion where
+-- | Benchmarks for generators
+module Data.GenValidity.Criterion
+  ( genValidityBench
+  , genUncheckedBench
+  , genValidBench
+  , genBench
+  ) where
 
 import Data.GenValidity
 import Data.Typeable
@@ -10,28 +16,28 @@ import Test.QuickCheck
 
 import Criterion
 
+-- | Benchmarks for both genUnchecked and genValid
 genValidityBench ::
-     forall a. (Typeable a, GenUnchecked a,GenValid a)
+     forall a. (Typeable a, GenUnchecked a, GenValid a)
   => Benchmark
 genValidityBench =
   bgroup (unwords ["GenValidity", nameOf @a]) [genValidBench @a, genUncheckedBench @a]
 
+-- | Benchmarks for both genUnchecked
 genUncheckedBench ::
      forall a. (Typeable a, GenUnchecked a)
   => Benchmark
-genUncheckedBench = genBench "genUnchecked" (genUnchecked @a)
+genUncheckedBench = genBench (unwords ["genUnchecked", nameOf @a]) (genUnchecked @a)
 
+-- | Benchmarks for both genValid
 genValidBench ::
      forall a. (Typeable a, GenValid a)
   => Benchmark
-genValidBench = genBench "genValid" (genValid @a)
+genValidBench = genBench (unwords ["genValid", nameOf @a]) (genValid @a)
 
-genBench ::
-     forall a. Typeable a
-  => String
-  -> Gen a
-  -> Benchmark
-genBench s gen = bench (unwords [s, nameOf @a]) $ whnfIO $ generate (resize 30 gen)
+-- | Benchmarks a generator with a given name
+genBench :: String -> Gen a -> Benchmark
+genBench name gen = bench name $ whnfIO $ generate (resize 30 gen)
 
 nameOf ::
      forall a. Typeable a
