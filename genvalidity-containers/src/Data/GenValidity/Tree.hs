dab9e4bdf967b6b72a958ebdf6632b54375a89b8
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-12-04 15:43:48 +0100

    Started on a better distribution for generating lists

--- genvalidity-containers/src/Data/GenValidity/Tree.hs
+++ genvalidity-containers/src/Data/GenValidity/Tree.hs
@@ -7,6 +7,8 @@ import Control.Applicative ((<*>))
 import Data.Functor ((<$>))
 #endif
 import Data.GenValidity
+import Data.List.NonEmpty (NonEmpty(..), (<|))
+import qualified Data.List.NonEmpty as NE
 import Data.Validity.Tree ()
 
 import Test.QuickCheck
@@ -14,38 +16,56 @@ import Test.QuickCheck
 import Data.Tree
 
 instance GenUnchecked a => GenUnchecked (Tree a) where
-    genUnchecked = genTreeOf genUnchecked
-    shrinkUnchecked (Node v ts) =
-        [Node v' ts' | (v', ts') <- shrinkUnchecked (v, ts)]
+  genUnchecked = genTreeOf genUnchecked
+  shrinkUnchecked (Node v ts) = [Node v' ts' | (v', ts') <- shrinkUnchecked (v, ts)]
 
 instance GenValid a => GenValid (Tree a) where
-    genValid = genTreeOf genValid
-    shrinkValid (Node v ts) = [Node v' ts' | (v', ts') <- shrinkValid (v, ts)]
+  genValid = genTreeOf genValid
+  shrinkValid (Node v ts) = [Node v' ts' | (v', ts') <- shrinkValid (v, ts)]
 
 -- | There should be at least one invalid element, either it's here or it's
 -- further down the tree.
 instance (GenUnchecked a, GenInvalid a) => GenInvalid (Tree a) where
-    genInvalid =
-        sized $ \n -> do
-            size <- upTo n
-            (a, b) <- genSplit size
-            oneof
-                [ Node <$> resize a genInvalid <*> resize b genUnchecked
-                , Node <$> resize a genUnchecked <*> resize b genInvalid
-                ]
-    shrinkInvalid (Node v ts) =
-        if isInvalid v
-            then Node <$> shrinkInvalid v <*> shrinkUnchecked ts
-            else Node <$> shrinkUnchecked v <*> shrinkInvalid ts
+  genInvalid =
+    sized $ \n -> do
+      size <- upTo n
+      (a, b) <- genSplit size
+      oneof
+        [ Node <$> resize a genInvalid <*> resize b genUnchecked
+        , Node <$> resize a genUnchecked <*> resize b genInvalid
+        ]
+  shrinkInvalid (Node v ts) =
+    if isInvalid v
+      then Node <$> shrinkInvalid v <*> shrinkUnchecked ts
+      else Node <$> shrinkUnchecked v <*> shrinkInvalid ts
 
 -- | Generate a tree of values that are generated as specified.
 --
 -- This takes the size parameter much better into account
 genTreeOf :: Gen a -> Gen (Tree a)
 genTreeOf func =
-    sized $ \n -> do
-        size <- upTo n
-        (a, b) <- genSplit size
-        value <- resize a func
-        forest <- resize b $ genListOf $ genTreeOf func
-        return $ Node value forest
+  sized $ \n -> do
+    ne <- genNonEmptyOf func
+    turnIntoTree ne
+ where
+        turnIntoTree :: NonEmpty a -> Gen (Tree a)
+        turnIntoTree (e :| es) = do
+          groups <- turnIntoGroups es
+          subtrees <- mapM turnIntoTree groups
+          pure (Node e subtrees)
+
+        turnIntoGroups :: [a] -> Gen [NonEmpty a]
+        turnIntoGroups = go []
+          where
+            go :: [a] -> [a] -> Gen [NonEmpty a]
+            go acc [] =
+              case NE.nonEmpty acc of
+                Nothing -> pure []
+                Just ne -> pure [ne]
+            go acc (e:es) =
+              frequency
+                [ ( 1
+                  , do rest <- go [] es
+                       pure ((e :| acc) : rest))
+                , (4, go (e : acc) es)
+                ]
--- genvalidity/src/Data/GenValidity/Utils.hs
+++ genvalidity/src/Data/GenValidity/Utils.hs
@@ -21,9 +21,15 @@ module Data.GenValidity.Utils
     , genSplit3
     , genSplit4
     , genSplit5
+    , genSplit6
+    , genSplit7
+    , genSplit8
     , arbPartition
     , shuffle
     , genListOf
+#if MIN_VERSION_base(4,9,0)
+    , genNonEmptyOf
+#endif
       -- ** Helper functions for implementing shrinking functions
     , shrinkTuple
     , shrinkT2
@@ -36,12 +42,16 @@ import Test.QuickCheck hiding (Fixed)
 import Data.List (sortBy)
 import Data.Ord (comparing)
 #endif
+#if MIN_VERSION_base(4,9,0)
+import Data.List.NonEmpty(NonEmpty(..))
+import qualified Data.List.NonEmpty as NE
+#endif
 
 #if MIN_VERSION_base(4,8,0)
-import Control.Monad (forM)
+import Control.Monad (forM, replicateM)
 #else
 import Control.Applicative ((<$>), (<*>), pure)
-import Control.Monad (forM)
+import Control.Monad (forM, replicateM)
 #endif
 -- | 'upTo' generates an integer between 0 (inclusive) and 'n'.
 upTo :: Int -> Gen Int
@@ -87,16 +97,65 @@ genSplit5 n
         (d, e) <- genSplit z
         return (a, b, c, d, e)
 
+-- | 'genSplit6 a' generates a sextuple '(b, c, d, e, f, g)' such that 'b + c + d + e + f + g' equals 'a'.
+genSplit6 :: Int -> Gen (Int, Int, Int, Int, Int, Int)
+genSplit6 n
+    | n < 0 = pure (0, 0, 0, 0, 0, 0)
+    | otherwise = do
+        (y, z) <- genSplit n
+        (a, b, c) <- genSplit3 y
+        (d, e, f) <- genSplit3 z
+        return (a, b, c, d, e, f)
+
+-- | 'genSplit7 a' generates a septtuple '(b, c, d, e, f, g)' such that 'b + c + d + e + f + g' equals 'a'.
+genSplit7 :: Int -> Gen (Int, Int, Int, Int, Int, Int, Int)
+genSplit7 n
+    | n < 0 = pure (0, 0, 0, 0, 0, 0, 0)
+    | otherwise = do
+        (y, z) <- genSplit n
+        (a, b, c) <- genSplit3 y
+        (d, e, f, g) <- genSplit4 z
+        return (a, b, c, d, e, f, g)
+
+-- | 'genSplit8 a' generates a octtuple '(b, c, d, e, f, g, h)' such that 'b + c + d + e + f + g + h' equals 'a'.
+genSplit8 :: Int -> Gen (Int, Int, Int, Int, Int, Int, Int, Int)
+genSplit8 n
+    | n < 0 = pure (0, 0, 0, 0, 0, 0, 0, 0)
+    | otherwise = do
+        (y, z) <- genSplit n
+        (a, b, c, d) <- genSplit4 y
+        (e, f, g, h) <- genSplit4 z
+        return (a, b, c, d, e, f, g, h)
+
+
 -- | 'arbPartition n' generates a list 'ls' such that 'sum ls' equals 'n'.
 arbPartition :: Int -> Gen [Int]
-arbPartition i = go i >>= shuffle
+arbPartition 0 = pure []
+arbPartition i = genLen i >>= go i
   where
-    go k
-        | k <= 0 = pure []
-        | otherwise = do
-            first <- choose (1, k)
-            rest <- arbPartition $ k - first
-            return $ first : rest
+    genLen :: Int -> Gen Int
+    genLen maxLen = round . invT (fromIntegral maxLen) <$> choose (0, 1)
+
+    invT :: Double -> Double -> Double
+    invT maxLen u =
+      let a = 1
+          b = maxLen
+          c = 2
+          fc = (c - a) / (b - a)
+      in if u < fc
+        then a + sqrt (u * (b - a) * (c - a) )
+        else b - sqrt ((1 - u) * (b - a) * (b - c))
+
+
+    go :: Int -> Int -> Gen [Int]
+    go size len = do
+      us <- replicateM len $ choose (0, 1)
+      let invs = map (invE 0.25) us
+      pure $ map (round . (* (fromIntegral size / sum invs))) invs
+
+    invE :: Double -> Double -> Double
+    invE lambda u = - log (1 - u) / lambda
+
 #if !MIN_VERSION_QuickCheck(2,8,0)
 -- | Generates a random permutation of the given list.
 shuffle :: [a] -> Gen [a]
@@ -104,6 +163,16 @@ shuffle xs = do
     ns <- vectorOf (length xs) (choose (minBound :: Int, maxBound))
     return (map snd (sortBy (comparing fst) (zip ns xs)))
 #endif
+
+#if MIN_VERSION_base(4,9,0)
+genNonEmptyOf :: Gen a -> Gen (NonEmpty a)
+genNonEmptyOf gen = do
+  l <- genListOf gen
+  case NE.nonEmpty l of
+    Nothing -> scale (+1) $ genNonEmptyOf gen
+    Just ne -> pure ne
+#endif
+
 -- | A version of @listOf@ that takes size into account more accurately.
 --
 -- This generator distributes the size that is is given among the values
