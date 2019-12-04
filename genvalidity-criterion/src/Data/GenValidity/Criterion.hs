056d7aab1cee052824aa6a5c1fec5b4788174fa8
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-12-04 23:46:01 +0100

    made the benchmarks require nfdata

--- genvalidity-criterion/src/Data/GenValidity/Criterion.hs
+++ genvalidity-criterion/src/Data/GenValidity/Criterion.hs
@@ -10,6 +10,7 @@ module Data.GenValidity.Criterion
   , genBench
   ) where
 
+import Control.DeepSeq
 import Data.GenValidity
 import Data.Typeable
 import Test.QuickCheck
@@ -18,28 +19,32 @@ import Criterion
 
 -- | Benchmarks for both genUnchecked and genValid
 genValidityBench ::
-     forall a. (Typeable a, GenUnchecked a, GenValid a)
+     forall a. (Typeable a, NFData a, GenUnchecked a, GenValid a)
   => Benchmark
 genValidityBench =
   bgroup (unwords ["GenValidity", nameOf @a]) [genValidBench @a, genUncheckedBench @a]
 
 -- | Benchmarks for both genUnchecked
 genUncheckedBench ::
-     forall a. (Typeable a, GenUnchecked a)
+     forall a. (Typeable a, NFData a, GenUnchecked a)
   => Benchmark
 genUncheckedBench = genBench (unwords ["genUnchecked", nameOf @a]) (genUnchecked @a)
 
 -- | Benchmarks for both genValid
 genValidBench ::
-     forall a. (Typeable a, GenValid a)
+     forall a. (Typeable a, NFData a, GenValid a)
   => Benchmark
 genValidBench = genBench (unwords ["genValid", nameOf @a]) (genValid @a)
 
 -- | Benchmarks a generator with a given name
-genBench :: String -> Gen a -> Benchmark
-genBench name gen = bench name $ whnfIO $ generate (resize 30 gen)
+genBench :: NFData a => String -> Gen a -> Benchmark
+genBench name gen = bench name $ nfIO $ generate (resize 30 gen)
 
 nameOf ::
      forall a. Typeable a
   => String
-nameOf = show $ typeRep (Proxy @a)
+nameOf =
+  let s = show $ typeRep (Proxy @a)
+  in if ' ' `elem` s
+  then "(" <> s <> ")"
+  else s
