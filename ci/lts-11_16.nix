4b2b5f8a787be11d52806bf32953aba4141dd540
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-01-05 21:08:40 +0100

    and again

--- ci/lts-11_16.nix
+++ ci/lts-11_16.nix
@@ -1,4 +1,4 @@
 {
   rev    = "bf9ad8da0749e59bffa3160488ae3ccadb0216a9";
-  sha256 = "03iyf4qszsrbslz8xayph4b8zp83naw3asma1png56k58vpcyjaa";
+  sha256 = "sha256:1jzyd8726n9hrvwcyllg6hh52carb09nhnjyv8drqrz71igvkdcs";
 }
