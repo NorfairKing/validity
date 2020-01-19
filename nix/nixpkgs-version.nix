165c75d350460c0f936600b17795ba40b298786b
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-01-19 15:20:06 +0100

    upgraded lts version

--- nix/nixpkgs-version.nix
+++ nix/nixpkgs-version.nix
@@ -1,7 +1,4 @@
-# To update:
-# nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/07723f14bb132f6d65c74b953bef39cd80457273.tar.gz
-
 {
-  rev    = "b77ed681562b4364ea6678d9bdd53e0707d4f509";
-  sha256 = "sha256:1y8qfpgz9xcchllxdqr40br3w4z2mvrg13zsf51b4zjwp757k52q";
+  rev = "323fe92057bb38475a446ff5e18d3b388c2e4403";
+  sha256 = "sha256:0hmjasnq0x40i2x22d4qrqf1hph4bwrjhx4zwz2s6nyqfqinbcyn";
 }
