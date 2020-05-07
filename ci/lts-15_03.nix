3d02fb8869e30402ccdf812dc87fb745ee678e42
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-05-07 16:13:52 +0200

    Added lts-15.3 to ci

--- ci/lts-15_03.nix
+++ ci/lts-15_03.nix
@@ -1,4 +1,4 @@
 {
   rev = "beeb24f1e939be7d85fdd64e31f13b8fe8238150";
-  sha256 = "sha256:0h8i0b2dyz8bppkfs5srkaq4hrc9ngb6idg7cq9ngvz3szr6xxaa";
+  sha256 = "sha256:1v62ggrhw9126vjwlffzz87gm7an1f459j5ap53x4ji8l18sy3ym";
 }
