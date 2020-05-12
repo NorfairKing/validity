d513259227ef8b0c8bb7a6c4eca85076f2fc0259
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-05-12 18:04:47 +0200

    validity-persistent

---
+++ validity-persistent/src/Data/Validity/Persist.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Data.Validity.Persist where
+
+import Data.Validity
+import Database.Persist
+
+instance Validity (Key a) where
+  validate = trivialValidation
+
+instance Validity a => Validity (Entity a) where
+  validate e = mconcat [delve "entityKey" $ entityKey e, delve "entityVal" $ entityVal e]
