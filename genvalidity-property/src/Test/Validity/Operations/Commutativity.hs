67841028f7fde8472c482840cf886f801effc45e
Artem Yurchenko <artemyurchenko@zoho.com>
2020-06-01 12:25:42 +0300

    fix 'commutative' to allow operations with result type different from argument types
    https://github.com/NorfairKing/validity/issues/81
    
    also add tests for some commutative operations

--- genvalidity-property/src/Test/Validity/Operations/Commutativity.hs
+++ genvalidity-property/src/Test/Validity/Operations/Commutativity.hs
@@ -22,8 +22,8 @@ import Test.QuickCheck
 --     a \star b = b \star a
 -- \]
 commutativeOnGens ::
-       (Show a, Eq a)
-    => (a -> a -> a)
+       (Show a, Show b, Eq b)
+    => (a -> a -> b)
     -> Gen (a, a)
     -> ((a, a) -> [(a, a)])
     -> Property
@@ -34,20 +34,27 @@ commutativeOnGens op gen s =
 --
 -- prop> commutativeOnValids ((+) :: Rational -> Rational -> Rational)
 -- prop> commutativeOnValids ((*) :: Rational -> Rational -> Rational)
-commutativeOnValids :: (Show a, Eq a, GenValid a) => (a -> a -> a) -> Property
+commutativeOnValids ::
+          (Show a, Show b, Eq b, GenValid a)
+       => (a -> a -> b) -> Property
 commutativeOnValids op = commutativeOnGens op genValid shrinkValid
 
 -- |
 --
 -- prop> commutative ((+) :: Int -> Int -> Int)
 -- prop> commutative ((*) :: Int -> Int -> Int)
-commutative :: (Show a, Eq a, GenUnchecked a) => (a -> a -> a) -> Property
+commutative ::
+          (Show a, Show b, Eq b, GenUnchecked a)
+       => (a -> a -> b) -> Property
 commutative op = commutativeOnGens op genUnchecked shrinkUnchecked
 
 -- |
 --
 -- prop> commutativeOnArbitrary ((+) :: Int -> Int -> Int)
 -- prop> commutativeOnArbitrary ((*) :: Int -> Int -> Int)
+-- commutativeOnArbitrary ::
+--        (Show a, Eq a, Arbitrary a) => (a -> a -> a) -> Property
 commutativeOnArbitrary ::
-       (Show a, Eq a, Arbitrary a) => (a -> a -> a) -> Property
+          (Show a, Show b, Eq b, Arbitrary a)
+       => (a -> a -> b) -> Property
 commutativeOnArbitrary op = commutativeOnGens op arbitrary shrink
