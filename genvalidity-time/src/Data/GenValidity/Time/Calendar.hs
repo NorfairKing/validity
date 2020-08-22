8438cbd75d80ca7fd6745074685e5003da78dac7
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-08-22 04:08:09 +0200

    much smarter day generation

--- genvalidity-time/src/Data/GenValidity/Time/Calendar.hs
+++ genvalidity-time/src/Data/GenValidity/Time/Calendar.hs
@@ -5,6 +5,8 @@ module Data.GenValidity.Time.Calendar where
 
 import Data.GenValidity
 import Data.Time.Calendar
+import System.IO.Unsafe
+import Data.Time
 import Data.Validity.Time.Calendar ()
 #if !MIN_VERSION_base(4,8,0)
 import Data.Functor ((<$>))
@@ -16,4 +18,32 @@ instance GenUnchecked Day where
   shrinkUnchecked _ = []
 
 instance GenValid Day where
-  genValid = (ModifiedJulianDay <$> genValid) `suchThat` isValid
+  genValid = oneof
+    [ (ModifiedJulianDay <$> genValid) `suchThat` isValid
+    , uniformlyOneHundredYearsAroundToday
+    , uniformlyThreeYearsAroundToday
+    , uniformlyDaysAroundToday
+    ]
+
+    where
+      uniformlyOneHundredYearsAroundToday = do
+        y <- choose (1970, 2070)
+        m <- choose (1, 12)
+        d <- choose (1, 31)
+        pure $ fromGregorian y m d
+
+      uniformlyThreeYearsAroundToday = do
+        let (thisYear, _, _) =  toGregorian today
+        y <- choose (pred thisYear, succ thisYear)
+        m <- choose (1, 12)
+        d <- choose (1, 31)
+        pure $ fromGregorian y m d
+
+      uniformlyDaysAroundToday = sized $ \s -> do
+        diff <- choose (0, s)
+        pure $ addDays (fromIntegral diff) today
+
+      -- This will make the code potentially flaky, but that's an okay tradeoff given that it
+      -- will generate days around the current day: A positive and negative diff around today.
+      today :: Day
+      today = unsafePerformIO $ utctDay <$> getCurrentTime
