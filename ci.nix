1c3671a662673e21a1c5e8056eef5a7b0e8720ea
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-06-05 14:11:41 +0200

    upgraded lts

--- ci.nix
+++ ci.nix
@@ -7,8 +7,6 @@ let
     "lts-14_23" = "a87b506140a7267477103759c3f8da5b2e8d994e";
     "lts-15_03" = "beeb24f1e939be7d85fdd64e31f13b8fe8238150";
     "lts-16_11" = "89db531aea80df58584c9a9e3504ffd9617e6b48";
-    "lts-16_20" = "35b3a1f43a9621a88e906a839f99d8252500152b";
-    "nightly-2021-03-01" = "0dff305a49a1ce72fda09206abbaff40ef41efd7";
   };
 
   mkReleaseForVersion = version: rev:
