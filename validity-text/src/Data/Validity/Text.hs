e57ee82e103bc63174b72a9016b3a00ae9b6474d
Bodigrim <andrew.lelechenko@gmail.com>
2021-11-23 22:51:51 +0000

    Support text-2.0

--- validity-text/src/Data/Validity/Text.hs
+++ validity-text/src/Data/Validity/Text.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Data.Validity.Text where
 
 import Control.Exception (evaluate, try)
+import Data.ByteString (ByteString)
 import qualified Data.ByteString.Builder as SBB
 import qualified Data.ByteString.Lazy as LB
+import Data.Text (Text)
 import qualified Data.Text as ST
 import qualified Data.Text.Array as A
 import qualified Data.Text.Encoding as E
@@ -13,33 +16,54 @@ import qualified Data.Text.Internal as ST
 import qualified Data.Text.Internal.Lazy as LT
 import qualified Data.Text.Unsafe as U
 import Data.Validity
+import Data.Word
 
 -- | A text is valid if the internal structure is consistent.
 instance Validity ST.Text where
   validate t@(ST.Text arr off len) =
     mconcat
-      [ check (len >= 0) "The length is positive.",
-        check (off >= 0) "The offset is positive.",
-        check
-          ( let c = A.unsafeIndex arr off
-             in len == 0 || c < 0xDC00 || c > 0xDFFF
-          )
-          "The offset character is valid UTF16.",
-        -- It contains a valid UTF16
-        check
-          ( (== (Right t :: Either E.UnicodeException ST.Text)) $
-              U.unsafeDupablePerformIO
-                . try
-                . evaluate
-                . E.decodeUtf16LEWith E.strictDecode
-                . LB.toStrict
-                . SBB.toLazyByteString
-                . mconcat
-                . map SBB.word16LE
-                $ A.toList arr off len
-          )
-          "The bytes can correctly be decoded as UTF16."
+      [ declare "The length is positive." (len >= 0),
+        declare "The offset is positive." (off >= 0),
+        declare "The offset char is valid" $
+          let c = A.unsafeIndex arr off
+           in len == 0 || offsetCharCheck c,
+        declare "The array contains bytes in the right encoding" $
+          (== (Right t :: Either E.UnicodeException ST.Text))
+            . U.unsafeDupablePerformIO
+            . try
+            . evaluate
+            . validityDecoding
+            . LB.toStrict
+            . SBB.toLazyByteString
+            . mconcat
+            . map validityWording
+            $ A.toList arr off len
       ]
+    where
+
+#if MIN_VERSION_text(2,0,0)
+      offsetCharCheck :: Word8 -> Bool
+      offsetCharCheck c =  c < 0x80 || c >= 0xC0 -- Valid UTF8
+#else
+      offsetCharCheck :: Word16 -> Bool
+      offsetCharCheck c = c < 0xDC00 || c > 0xDFFF -- Valid UTF16
+#endif
+
+#if MIN_VERSION_text(2,0,0)
+      validityDecoding :: ByteString -> Text
+      validityDecoding = E.decodeUtf8With E.strictDecode
+#else
+      validityDecoding :: ByteString -> Text
+      validityDecoding = E.decodeUtf16LEWith E.strictDecode
+#endif
+
+#if MIN_VERSION_text(2,0,0)
+      validityWording :: Word8 -> SBB.Builder
+      validityWording = SBB.word8
+#else
+      validityWording :: Word16 -> SBB.Builder
+      validityWording = SBB.word16LE
+#endif
 
 -- | A lazy text value is valid if all the internal chunks are valid and nonempty
 instance Validity LT.Text where
