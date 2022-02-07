258414b8e2341cd92a859af7f9d0e847c13dba64
Sridhar Ratnakumar <srid@srid.ca>
2022-02-07 15:11:00 -0500

    lts 19.3
    - Sized Word/Int changes: https://gitlab.haskell.org/ghc/ghc/-/wikis/migration/9.2#sized-wordint-use-sized-wordint-primitives
    - Aeson 2's KeyMap/Key types are new: https://github.com/haskell/aeson/issues/881#issuecomment-944920664

--- genvalidity-aeson/src/Data/GenValidity/Aeson.hs
+++ genvalidity-aeson/src/Data/GenValidity/Aeson.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Data.GenValidity.Aeson where
 
 import Data.Aeson
+#if MIN_VERSION_aeson(2,0,0)
+import Data.Aeson.KeyMap (KeyMap)
+import qualified Data.Aeson.KeyMap as KM
+import qualified Data.Aeson.Key as K
+#endif
 import Data.Foldable (toList)
 import Data.GenValidity
 import Data.GenValidity.HashMap ()
@@ -11,6 +17,16 @@ import Data.GenValidity.Text ()
 import Data.GenValidity.Vector ()
 import Data.Validity.Aeson ()
 import Test.QuickCheck
+
+#if MIN_VERSION_aeson(2,0,0)
+instance GenValid Key where
+  genValid = K.fromString <$> genValid
+  shrinkValid = fmap K.fromString . shrinkValid . K.toString
+
+instance (GenValid v) => GenValid (KeyMap v) where
+  genValid = KM.fromList <$> genValid
+  shrinkValid = fmap KM.fromList . shrinkValid . KM.toList
+#endif
 
 instance GenValid Value where
   genValid =
