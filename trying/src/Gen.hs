a2c531c5635cf6e03799d02fa4dee04f8f1bd108
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-04-24 14:11:22 +0200

    figured out some shrinking

--- trying/src/Gen.hs
+++ trying/src/Gen.hs
@@ -1,11 +1,22 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Gen where
 
+import Control.Monad
+import Data.Maybe
 import Data.Validity
 import Data.Vector.Unboxed (Vector)
 import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector.Unboxed.Mutable as MUV
 import Data.Word
 import Debug.Trace
 import System.Random.SplitMix as SM
@@ -36,14 +47,7 @@ runGen = flip go
       GenPure a -> a
       GenFMap f g' -> f $ go ws g'
       GenAp ff fa ->
-        let len = UV.length ws
-            (leftWs, rightWs) = case len of
-              0 -> (UV.empty, UV.empty)
-              1 -> (UV.empty, UV.empty)
-              n ->
-                let leftSize = computeSplit (pred len) (UV.head ws)
-                    restRandomness = UV.tail ws
-                 in (UV.take leftSize ws, UV.drop leftSize ws)
+        let (leftWs, rightWs) = computeSplitRandomness ws
          in (go leftWs ff) (go rightWs fa)
 
 -- | Compute an arbitrarily split value
@@ -57,6 +61,17 @@ computeSplit :: Int -> Word64 -> Int
 computeSplit totalSize randomWord =
   let left = randomWord `rem` (fromIntegral (totalSize + 1))
    in fromIntegral left
+
+computeSplitRandomness :: Vector Word64 -> (Vector Word64, Vector Word64)
+computeSplitRandomness ws =
+  let len = UV.length ws
+   in case len of
+        0 -> (UV.empty, UV.empty)
+        1 -> (UV.empty, UV.empty)
+        n ->
+          let leftSize = computeSplit (pred len) (UV.head ws)
+              restRandomness = UV.tail ws
+           in UV.splitAt leftSize restRandomness
 
 -- | Compute an arbitrary partition
 --
@@ -119,23 +134,152 @@ computeAllShrinks v =
     then []
     else
       let vs = shrinkRandomness v
-       in if null vs
-            then []
-            else concatMap computeAllShrinks vs ++ vs
+       in case listToMaybe vs of
+            Nothing -> []
+            Just v' -> v' : computeAllShrinks v'
 
 -- Compute the next shrinking steps.
 --
 -- Laws:
 --
 -- 1. Must never shrink to itself.
+--
+-- These needs to be in order of "must shrinking progress first"
 shrinkRandomness :: Vector Word64 -> [Vector Word64]
-shrinkRandomness v =
-  if UV.null v
-    then []
-    else [UV.init v, UV.tail v]
+shrinkRandomness ws =
+  concat
+    [ every tryToLog,
+      every tryToSqrt,
+      every tryToDivideByTwo,
+      every tryToPred,
+      [UV.tail ws | not (UV.null ws)],
+      [UV.init ws | not (UV.null ws)],
+      each tryToLog,
+      each tryToSqrt,
+      each tryToDivideByTwo,
+      each tryToPred
+    ]
+  where
+    -- TODO keep a cache of the randomnesses we've
+    -- already tried
+    every :: (Word64 -> Maybe Word64) -> [Vector Word64]
+    every fun =
+      let ws' = UV.map (\v -> fromMaybe v $ fun v) ws
+       in [ws' | ws /= ws']
+    each :: (Word64 -> Maybe Word64) -> [Vector Word64]
+    each fun =
+      mapMaybe
+        ( \ix -> do
+            let v = UV.unsafeIndex ws ix
+            v' <- fun v
+            pure $
+              UV.modify
+                ( \mv ->
+                    MUV.write mv ix v'
+                )
+                ws
+        )
+        [0 .. UV.length ws - 1]
+    tryToLog = \case
+      0 -> Nothing
+      1 -> Nothing
+      w -> Just $ floor $ logBase 2 $ fromIntegral w
+    tryToSqrt = \case
+      0 -> Nothing
+      1 -> Nothing
+      w -> Just $ floor $ sqrt $ fromIntegral w
+    tryToDivideByTwo = \case
+      0 -> Nothing
+      w -> Just $ w `div` 2
+    tryToPred = \case
+      0 -> Nothing
+      w -> Just $ pred w
 
--- Compute the next shrinking step for this value.
-shrinkSingleWord64 :: Word64 -> [Word64]
-shrinkSingleWord64 w =
-  let log = logBase 2 (fromIntegral w)
-   in []
+data Property ls where
+  PropBool :: Bool -> Property '[]
+  PropGen :: Gen a -> (a -> Property ls) -> Property (a ': ls)
+
+data family PList (l :: [*])
+
+data instance PList '[] = PNil
+
+data instance PList (x ': xs) = x `PCons` PList xs
+
+deriving instance Show (PList '[])
+
+deriving instance (Show x, Show (PList xs)) => Show (PList (x ': xs))
+
+deriving instance Eq (PList '[])
+
+deriving instance (Eq x, Eq (PList xs)) => Eq (PList (x ': xs))
+
+deriving instance Ord (PList '[])
+
+deriving instance (Ord x, Ord (PList xs)) => Ord (PList (x ': xs))
+
+runProperty ::
+  Vector Word64 ->
+  Property ls ->
+  (PList ls, Bool)
+runProperty = go
+  where
+    go :: Vector Word64 -> Property ls -> (PList ls, Bool)
+    go ws = \case
+      PropBool b -> (PNil, b)
+      PropGen gen func ->
+        let (usedRandomness, restRandomness) = computeSplitRandomness ws
+            value = runGen gen usedRandomness
+            (generateds, result) = go restRandomness (func value)
+         in (PCons value generateds, result)
+
+shrinkProperty ::
+  forall ls.
+  Show (PList ls) =>
+  Vector Word64 ->
+  Property ls ->
+  Maybe (PList ls)
+shrinkProperty r prop =
+  case shrinkPropertyAsMuchAsPossible r prop of
+    [] -> Nothing
+    ls -> Just $ last ls
+
+shrinkPropertyAsMuchAsPossible ::
+  forall ls.
+  Show (PList ls) =>
+  Vector Word64 ->
+  Property ls ->
+  [PList ls]
+shrinkPropertyAsMuchAsPossible r prop = go r
+  where
+    run :: Vector Word64 -> (PList ls, Bool)
+    run ws' = runProperty ws' prop
+    go :: Vector Word64 -> [PList ls]
+    go ws =
+      traceShow ws $
+        let shrinks = shrinkPropertyOneStep ws prop
+         in case listToMaybe shrinks of
+              Nothing -> []
+              Just (ws', values) ->
+                trace ("Successfully shrunk " <> show ws <> " to " <> show ws' <> " yielding " <> show values) $
+                  traceShowId values : go ws'
+
+shrinkPropertyOneStep ::
+  Show (PList ls) =>
+  Vector Word64 ->
+  Property ls ->
+  [(Vector Word64, PList ls)]
+shrinkPropertyOneStep ws prop = do
+  ws' <- shrinkRandomness ws
+  let (vals, result) = runProperty ws' prop
+  traceShowM (ws', vals, result)
+  guard $ not result
+  pure (ws', vals)
+
+class IsProperty ls a where
+  toProperty :: a -> Property ls
+
+instance IsProperty '[] Bool where
+  toProperty = PropBool
+
+instance (GenValid a, IsProperty ls b) => IsProperty (a ': ls) (a -> b) where
+  toProperty func = PropGen genValid $ \a -> toProperty (func a)
--- trying/test/GenSpec.hs
+++ trying/test/GenSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
@@ -16,45 +17,78 @@ spec = do
     it "shrinks to 0" $
       computeSplit 30 0 `shouldBe` 0
 
-  describe "computePartition" $ do
+  xdescribe "computePartition" $ do
     it "shrinks to [] for a zero size" $
       computePartition 0 0 `shouldBe` []
     it "shrinks to [size] for a nonzero size" $
       computePartition 30 0 `shouldBe` [30]
 
-  describe "shrinkRandomness" $
+  describe "shrinkRandomness" $ do
     it "does not shrink an empty vector" $
-      shrinkRandomness UV.empty `shouldBe` []
+      shrinkRandomness UV.empty
+        `shouldBe` []
+    it "always tries to shrink to a smaller vector from the beginning" $
+      let shrinks = shrinkRandomness $ UV.fromList [1, 2, 3]
+       in shrinks `shouldSatisfy` (UV.fromList [2, 3] `elem`)
+    it "always tries to shrink to a smaller vector from the end" $
+      let shrinks = shrinkRandomness $ UV.fromList [1, 2, 3]
+       in shrinks `shouldSatisfy` (UV.fromList [1, 2] `elem`)
 
-  describe "shrinkSingleWord64" $
-    it "does not shrink 0" $
-      shrinkSingleWord64 0 `shouldBe` []
+  describe "runProperty" $ do
+    shrinksToSpec
+      -- \w -> w < 1
+      (PropGen genValid (\w -> PropBool ((w :: Word8) < 1)))
+      (PCons 1 PNil)
+    shrinksToSpec
+      -- \w1 -> w2 -> w1 + w2 < 2
+      (PropGen genValid (\w1 -> (PropGen genValid (\w2 -> PropBool ((w1 + w2 :: Word8) < 2)))))
+      (PCons 1 (PCons 1 PNil))
+    shrinksToSpec
+      -- \w1 -> w2 -> w1 >= w2
+      (PropGen genValid (\w1 -> (PropGen genValid (\w2 -> PropBool (w1 >= (w2 :: Word8))))))
+      (PCons 0 (PCons 1 PNil))
 
   describe "runGen" $ do
     goldenGenSpec @Bool "bool"
     goldenGenSpec @Word8 "word8"
     goldenGenSpec @(Word8, Word8) "tuple-word8-word8"
-    goldenGenSpec @[Word8] "tuple-word8-word8"
+    -- goldenGenSpec @[Word8] "tuple-word8-word8"
     goldenGenSpec @Word64 "word64"
+
+shrinksToSpec ::
+  (Show (PList ls), Eq (PList ls)) =>
+  Property ls ->
+  PList ls ->
+  Spec
+shrinksToSpec property shrunk = do
+  let randomness = computeRandomness 100 32
+  it "fails the property" $
+    let (_, result) = runProperty randomness property
+     in result `shouldBe` False
+
+  it ("shrinks to " <> show shrunk) $
+    case shrinkProperty randomness property of
+      Nothing -> expectationFailure "should have been able to shrink"
+      Just values -> values `shouldBe` shrunk
 
 goldenGenSpec :: forall a. (Show a, GenValid a) => FilePath -> Spec
 goldenGenSpec fp = do
   let nbValues = 100
       sizes = [0 .. nbValues]
       seeds = [42 ..]
-  it "generates the same values" $ do
+  it ("generates the same " <> fp <> " values") $ do
     let randomnesses = zipWith computeRandomness sizes seeds
         values :: [a]
         values = map (runGen genValid) randomnesses
     pureGoldenStringFile ("test_resources/gen/" <> fp <> ".txt") $
       unlines $
         map show values
-  it "shrinks to the same Word64 values" $ do
+  it ("shrinks to the same " <> fp <> " values") $ do
     let randomness = computeRandomness nbValues 42
         maxValue :: a
         maxValue = runGen genValid randomness
         shrinks :: [a]
-        shrinks = map (runGen genValid) $ take 100 $ computeAllShrinks randomness
+        shrinks = map (runGen genValid) $ take 10 $ computeAllShrinks randomness
     pureGoldenStringFile ("test_resources/shrink/" <> fp <> ".txt") $
       unlines $
         show maxValue
